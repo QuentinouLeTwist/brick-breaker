f9d26f963b3910147648c9d3fbeed287bcdaf34a
QuentinouLeTwist <quentinmousset@gmail.com>
2018-01-28 00:41:59 +0100

    fix display of score

--- src/services/BannerCreator.ts
+++ src/services/BannerCreator.ts
@@ -15,42 +15,40 @@ export default class BannerCreator {
     };
   }
 
-  createCentered({ text, color }) {
+  createCentered(text: string, color: string, font?: string, fontSize?: number) {
     let banner = this.createBanner(this.context.game.world.centerX, this.context.game.world.centerY, text);
     banner.padding.set(10, 16);
     banner.fill = color;
     banner.smoothed = true;
     banner.anchor.setTo(0.5);
 
-    return this.setFontBanner(banner);
+    return this.setFontBanner(banner, font, fontSize);
   }
 
-  createAtPosition({ x, y, text, color }) {
+  createAtPosition(x: number, y: number, text: string, color: string, font?: string, fontSize?: number) {
+    if (font === undefined || font === '') {
+      font = this.fontOptions.font;
+    }
+    if (fontSize === undefined || fontSize === 0) {
+      fontSize = this.fontOptions.fontSize;
+    }
     let banner = this.createBanner(x, y, text);
     banner.padding.set(10, 16);
     banner.fill = color;
     banner.smoothed = true;
     banner.anchor.setTo(0.5);
 
-    return this.setFontBanner(banner);
+    return this.setFontBanner(banner, font, fontSize);
   }
 
-  setFont(font: string) {
-    this.fontOptions.font = font;
-  }
-
-  setFontSize(fontSize: number) {
-    this.fontOptions.fontSize = fontSize;
-  }
-
-  private setFontBanner(banner: Phaser.Text) {
-    banner.font = this.fontOptions.font;
-    banner.fontSize = this.fontOptions.fontSize;
+  setFontBanner(banner: Phaser.Text, font?: string, fontSize?: number) {
+    banner.font = font;
+    banner.fontSize = fontSize;
 
     return banner;
   }
 
-  private createBanner(x, y, text) {
+  createBanner(x: number, y: number, text: string) {
     return this.context.game.add.text(x, y, text, null);
   }
 
